902ad55532ba4626d3a352880c3f8ec4494d4633
shuaibiLei1 <2646785151@qq.com>
2022-04-21 13:45:51 +0800

    [Base] setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  data(){
+    return {
+      msg: "王彭磊",
+    }
+  },
+  created(){
+    
+  },
+}
+</script>
